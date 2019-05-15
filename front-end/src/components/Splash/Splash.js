e0ff07934b2abbb8261ec6776a872e8475b41887
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-15 11:57:31 -0400

    added some silly colors to show clear containers for each section on the splash

--- front-end/src/components/Splash/Splash.js
+++ front-end/src/components/Splash/Splash.js
@@ -9,7 +9,7 @@ const Splash = props => {
             </div>
             <div className={styles.middleSection}>
                 <h3>What is MTG Deck Builder?</h3>
-                <p>Informational Text here</p>
+                <p>Bacon ipsum dolor amet pork chop sausage sirloin turkey prosciutto beef chuck leberkas fatback kevin frankfurter drumstick ribeye. T-bone jerky drumstick porchetta cupim. Meatloaf capicola turkey jerky tongue. Picanha kielbasa tail alcatra swine. Shank chuck spare ribs, burgdoggen swine beef ribs brisket biltong bresaola meatball turkey. Prosciutto ham doner, meatball tail beef andouille turducken hamburger porchetta pork belly fatback ham hock. Turkey short ribs tongue doner spare ribs.</p>
             </div>
             <div className={styles.bottomSection}>
                 <h3>Interested?</h3>
