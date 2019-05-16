21a610f460a02ada594ecec3af6f311e85e33d25
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-16 15:51:59 -0400

    totally stumped as to how to design the front end right now"

--- front-end/src/components/Splash/Splash.js
+++ front-end/src/components/Splash/Splash.js
@@ -9,46 +9,48 @@ const Splash = props => {
                 <p>A deck building tool for Magic The Gathering's Commander Format</p>
             </div>
             <div className={styles.middleSection}>
+
                 <div className={styles.appDescription}>
                     <div className={styles.middleSectionText}>
-                        <h3>What is Dek Tech?</h3>
-                        <p>Dek Tech is an app that will revolutize how you approach deck building in Magic The Gathering's Commander format.</p>
-                    </div>
-                    <div className={styles.middleSectionImage}>
-                        <img className={styles.middleImage}src="https://via.placeholder.com/200" alt="placeholder"/>
+                        <h2>What is Dek Tech?</h2>
+                        <p>Dek Tech is an app that will revolutize how you approach deck building in Magic The Gathering's Commander format. Dek Tech is meant to serve as a one stop shop for searching for cards, creating decks and providing prices for each card so you may more knowledgably build decks that follow your budget!</p>
                     </div>
                 </div>
-                <div className={styles.appInstructionsStepOne}>
-                    <div className={styles.middleSectionText}>
-                        <h3>Create An Account</h3>
-                        <p>Instructions here</p>
+
+                <div className={styles.appSteps}>
+                    <h2>How Does It Work?</h2>
+                    <div className={styles.appInstructionsStepOne}>
+                        <div className={styles.middleSectionText}>
+                            <h3>Create An Account</h3>
+                            <p>Instructions here</p>
+                        </div>
+                        <div className={styles.middleSectionImage}>
+                            <img className={styles.middleImage}src="https://via.placeholder.com/150" alt="placeholder"/>
+                        </div>
                     </div>
-                    <div className={styles.middleSectionImage}>
-                        <img className={styles.middleImage}src="https://via.placeholder.com/200" alt="placeholder"/>
+                    <div className={styles.appInstructionsStepTwo}>
+                        <div className={styles.middleSectionText}>
+                            <h3>Create A Deck</h3>
+                            <p>Instructions here</p>
+                        </div>
+                        <div className={styles.middleSectionImage}>
+                            <img className={styles.middleImage}src="https://via.placeholder.com/150" alt="placeholder"/>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.appInstructionsStepTwo}>
-                    <div className={styles.middleSectionText}>
-                        <h3>Create A Deck</h3>
-                        <p>Instructions here</p>
+                    <div className={styles.appInstructionsStepThree}>
+                        <div className={styles.middleSectionText}>
+                            <h3>Start Searching and Adding Cards!</h3>
+                            <p>Instructions here</p>
+                        </div>
+                        <div className={styles.middleSectionImage}>
+                            <img className={styles.middleImage}src="https://via.placeholder.com/150" alt="placeholder"/>
+                        </div>
                     </div>
-                    <div className={styles.middleSectionImage}>
-                        <img className={styles.middleImage}src="https://via.placeholder.com/200" alt="placeholder"/>
-                    </div>
-                </div>
-                <div className={styles.appInstructionsStepThree}>
-                    <div className={styles.middleSectionText}>
-                        <h3>Start Searching and Adding Cards!</h3>
-                        <p>Instructions here</p>
-                    </div>
-                    <div className={styles.middleSectionImage}>
-                        <img className={styles.middleImage}src="https://via.placeholder.com/200" alt="placeholder"/>
-                    </div>
-                </div>
+                </div>     
             </div>
             <div className={styles.bottomSection}>
-                <h3>Interested?</h3>
-                <p>Information about registering</p>
+                <h2>Interested?</h2>
+                <p>Feel free to click the link HERE to register or login! Happy Deck Building!!!</p>
             </div>
         </div>
     )
