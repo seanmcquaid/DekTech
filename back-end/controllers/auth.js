45bf2b6f21e52e76ed316284799e34b7d667cf37
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-23 19:46:52 -0400

    trying to implement passport still with jwt

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 const nodemailer = require("nodemailer");
 const sendGridTransport = require("nodemailer-sendgrid-transport");
 const config = require("../config");
-const passport = require("../passport/passport");
+const passport = require("../authentication/passport");
 
 const transporter = nodemailer.createTransport(sendGridTransport({
     auth: {
@@ -10,44 +10,10 @@ const transporter = nodemailer.createTransport(sendGridTransport({
     }
 }));
 
-exports.postLogin = (req, res, next) => {
-    console.log('routes/user.js, login, req.body: ');
-    console.log(req.body)
-    next()
+exports.postLogin = (req,res,next) => {
+
 }
 
-exports.passPortLogin = passport.authenticate('local'), (req, res) => {
-    console.log('logged in', req.user);
-    var userInfo = {
-        username: req.user.username
-    };
-    res.json(userInfo);
-};
-
 exports.postRegister = (req,res,next) => {
-    const {username, password} = req.body;
-    // console.log(username, password)
-    User.findOne({username : username})
-        .then(user => {
-            if(user){
-                return res.json({
-                    message : "User already exists",
-                    loggedIn : false
-                });
-            }
-            const newUser = new User({
-                username : username,
-                password : password
-            });
-            return newUser.save()
-                .then((user)=>{
-                    return res.json({
-                        message : "Successfully registered",
-                        loggedIn : true
-                    })
-                });
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    
 };
--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require("../controllers/auth");
-const passport = require("../passport/passport");
+const passport = require("../authentication/passport");
 
 /* GET users listing. */
-router.post('/login', (req, res, next) => {
-    // console.log('routes/user.js, login, req.body: ');
-    // console.log(req.body)
-    next()
-},
-passport.authenticate('local'), (req, res) => {
-    console.log('logged in', req.user);
-    const userInfo = {
-        username: req.user.username
-    };
-    res.json(userInfo);
-});
+router.post('/login', authcontroller.postLogin);
 
 router.post("/register", authController.postRegister);
 
