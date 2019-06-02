ce17e99be0260f0f2ff54185d3c340dadc14d265
seanmcquaid <sean.mcquaid2@verizon.net>
2019-06-02 00:02:53 -0400

    made logged in logic with jwt

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -12,28 +12,72 @@ const transporter = nodemailer.createTransport(sendGridTransport({
 }));
 
 exports.postLogin = (req,res,next) => {
-    
+    const {username, password} = req.body;
+    if(!username || !password){
+        return res.status(400).json({
+            message : "Please enter all fields!",
+            loggedIn : false,
+        });
+    }
+
+    User.findOne({username})
+        .then(user => {
+            if(!user){
+                return res.status(400).json({
+                    message : "User doesn't exist!",
+                    loggedIn : false,
+                });
+            }
+
+            return bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(!isMatch){
+                        return res.status(400).json({
+                            message : "Invalid password!",
+                            loggedIn : false,
+                        });
+                    };
+
+                    const token = jwt.sign(
+                        {id : user._id},
+                        config.jwtSecret,
+                        {expiresIn: 3600}
+                    );
+
+                    return res.json({
+                        token,
+                        userInfo : user,
+                        loggedIn : true
+                    });
+
+                })
+
+        })
+        .catch(err => console.log(err));
+
 };
 
 exports.postRegister = (req,res,next) => {
     const {username, password} = req.body;
     if(!username || !password){
         return res.status(400).json({
-            message : "Be sure to fill out all fields!"
+            message : "Be sure to fill out all fields!",
+            loggedIn : false,
         });
     }
     User.findOne({username})
         .then(user => {
             if(user){
                 return res.status(400).json({
-                    message : "User already exists!"
+                    message : "User already exists!",
+                    loggedIn : false,
                 });
             }
             return bcrypt.hash(password, 12)
                         .then(hashedPassword => {
                             const newUser = new User({
                                 username : username,
-                                password : hashedPassword
+                                password : hashedPassword,
                             });
                             newUser.save();
                             const token = jwt.sign(
@@ -44,7 +88,7 @@ exports.postRegister = (req,res,next) => {
                             res.json({
                                 token,
                                 userInfo : newUser,
-                                loggedIn : true
+                                loggedIn : true,
                             });
                         })
         })
