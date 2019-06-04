8e3dac1e3701f7ba2167fceaf94f929524f984f0
seanmcquaid <sean.mcquaid2@verizon.net>
2019-06-03 21:26:35 -0400

    after some thought, I will probably revert to the old store setu p with redux promise instead of thunk as it allows me to more clearly set my actions as props

--- front-end/src/actions/authActions/authActions.js
+++ front-end/src/actions/authActions/authActions.js
@@ -39,7 +39,7 @@ export const loadUser = (dispatch, getState) => {
             })
         })
         .catch(err => {
-            dispatch(returnErrors(err.response.statuserr.response.status));
+            dispatch(returnErrors(err.response.data, err.response.status));
             dispatch({
                 type : AUTH_ERROR
             })
