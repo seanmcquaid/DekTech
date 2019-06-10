3ac0cb764589dc0a37887873d891fe9d83bd236c
seanmcquaid <sean.mcquaid2@verizon.net>
2019-06-09 23:18:00 -0400

    starting card search component

---
+++ front-end/src/containers/CardSearch/CardSearch.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import styles from "./CardSearch.module.css";
+
+class CardSearch extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+
+
+
+
+    render(){
+        return (
+            <div>Hello!</div>
+        )
+    }
+}
+
+export default CardSearch;
