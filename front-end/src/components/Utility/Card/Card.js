08e6ce7c0fba5bd8ad39afe3356dfc7b7f566fac
seanmcquaid <sean.mcquaid2@verizon.net>
2019-06-09 23:49:39 -0400

    finished card component

---
+++ front-end/src/components/Utility/Card/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Card.module.css";
+import {Link} from "react-router-dom";
+
+const Card = (props) => {
+    return (
+        <div className={styles.cardContainer}>
+            <h5>{props.cardName}</h5>
+            <Link to={`/cardInfo/${props.cardId}`}>
+                <img src={props.imageUrl} alt={props.cardName}/>
+            </Link>
+            <div className={styles.buttonContainer}>
+                <Link to={`/cardInfo/${props.cardId}`}>View Card</Link>
+                <button onClick={props.clicked}>Add To Deck</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
