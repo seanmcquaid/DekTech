1a0201c490b44649444dbb7832026385c8227394
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-13 13:12:39 -0400

    fixing protected routing further

--- front-end/src/components/Routing/ProtectedRoutes/ProtectedRoutes.js
+++ front-end/src/components/Routing/ProtectedRoutes/ProtectedRoutes.js
@@ -11,17 +11,16 @@ import ErrorPage from "../../ErrorPage/ErrorPage";
 import {connect} from "react-redux";
 
 const ProtectedRoutes = props => {
-    console.log(props)
-    const protectedRoute = (component) => props.auth.isAuthenticated? component : <Redirect to="/"/>;
+    const protectedRoute = (Component) => { return props.auth.isAuthenticated? Component : () => <Redirect to="/"/>};
     return(
         <Switch>
             <Route exact path="/" component={Splash}/>
             <Route exact path="/register" component={Register}/>
             <Route exact path="/login" component={Login}/>
-            <Route exact path="/userHome" component={() => protectedRoute(UserHome)}/>
-            <Route exact path="/currentDeck" component={() => protectedRoute(CurrentDeck)}/>
-            <Route exact path="/cardSearch" component={() => protectedRoute(CardSearch)}/>
-            <Route exact path={`/cardInfo/:cardId`} component={() => protectedRoute(CardInfo)}/>
+            <Route exact path="/userHome" component={protectedRoute(UserHome)}/>
+            <Route exact path="/currentDeck" component={protectedRoute(CurrentDeck)}/>
+            <Route exact path="/cardSearch" component={protectedRoute(CardSearch)}/>
+            <Route exact path={`/cardInfo/:cardId`} component={protectedRoute(CardInfo)}/>
             <Route component={ErrorPage}/>
         </Switch>
     )
